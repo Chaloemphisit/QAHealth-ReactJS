116bcccfac77ae002b77fc2e4aa96ebc48a2454c
Chaloemphisit Sirichai <Chaloemphisit@users.noreply.github.com>
2018-10-15 01:07:15 +0700

    fix button color in 404 page

--- src/containers/Page404.js
+++ src/containers/Page404.js
@@ -8,7 +8,7 @@ export default class Page404 extends React.Component {
         <div class="fof">
           <h1>Error 404</h1>
           <h3>No match found for <code>{window.location.pathname}</code></h3>
-          <a class="button3"><Link to="/" className="back-404page">กลับหน้าหลัก</Link></a>
+          <div class="button3"><Link to="/" className="back404page">กลับหน้าหลัก</Link></div>
         </div>
       </div>
     );
