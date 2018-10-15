97085493d69766a18e37e4b0901bc8edb1024ba7
BantitaPot <44106639+BantitaPot@users.noreply.github.com>
2018-10-15 15:51:28 +0700

    add contact us

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import {Home, Page404, Ask} from '../containers'
+import {Home, Page404, Ask,ContactUs} from '../containers'
 
-const ContactUs = () => <h1 >อย่าพึ่งรีบดูสิ!!....กำลังทำอยู่</h1>
+//const ContactUs = () => <h1 >อย่าพึ่งรีบดูสิ!!....กำลังทำอยู่</h1>
 const LoginSignup = () => <h1 >อย่าพึ่งรีบดูสิ!!....กำลังทำอยู่</h1>
 
 export default () => (
